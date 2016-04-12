aa05b7c6c739f8d4dd96b0134824ad2c632215c6
grant@iowntheinter.net <grant@iowntheinter.net>
2016-04-12 13:15:33 -0700

    cluster manager is the real cluster manager

--- src/main/groovy/net/iowntheinter/vertx/coreLauncher/cluster/zookeeperVertxStarter.groovy
+++ src/main/groovy/net/iowntheinter/vertx/coreLauncher/cluster/zookeeperVertxStarter.groovy
@@ -4,6 +4,8 @@ import io.vertx.core.AbstractVerticle
 import io.vertx.core.AsyncResult
 import io.vertx.core.Vertx
 import io.vertx.core.VertxOptions
+import io.vertx.core.spi.cluster.ClusterManager
+import io.vertx.spi.cluster.impl.zookeeper.ZookeeperClusterManager
 import net.iowntheinter.vertx.coreLauncher.cluster.impl.embeddedZookeeper
 import net.iowntheinter.vertx.coreLauncher.coreStarter
 import net.iowntheinter.vertx.util.resourceLoader
@@ -43,6 +45,23 @@ class zookeeperVertxStarter {
 
     void start(VertxOptions opts) {
         startZk()
+
+        Properties zkConfig = new Properties();
+        zkConfig.setProperty("hosts.zookeeper", "127.0.0.1");
+        zkConfig.setProperty("path.root", "io.vertx");
+        zkConfig.setProperty("retry.initialSleepTime", "1000");
+        zkConfig.setProperty("retry.intervalTimes", "3");
+
+        ClusterManager mgr = new ZookeeperClusterManager(zkConfig);
+        VertxOptions options = new VertxOptions().setClusterManager(mgr);
+
+        Vertx.clusteredVertx(options, {res ->
+            if (res.succeeded()) {
+                Vertx vertx = res.result();
+            } else {
+                // failed!
+            }
+        });
       //  vertx = Vertx.clusteredVertx(opts, startupResult)
     }
     def startupResult = { AsyncResult res ->
