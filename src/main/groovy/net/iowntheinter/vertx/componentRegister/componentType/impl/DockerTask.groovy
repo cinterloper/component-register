8d13faeff5b6837b37644208cafc7ec3cd434e14
grant@iowntheinter.net <grant@iowntheinter.net>
2016-04-13 23:16:33 -0700

    update

--- src/main/groovy/net/iowntheinter/vertx/componentRegister/componentType/impl/DockerTask.groovy
+++ src/main/groovy/net/iowntheinter/vertx/componentRegister/componentType/impl/DockerTask.groovy
@@ -1,6 +1,10 @@
 package net.iowntheinter.vertx.componentRegister.componentType.impl
 
-import de.gesellix.docker.client.DockerClientImpl
+import com.spotify.docker.client.DefaultDockerClient
+import com.spotify.docker.client.DockerClient
+import com.spotify.docker.client.messages.ContainerConfig
+import com.spotify.docker.client.messages.ContainerCreation
+import com.spotify.docker.client.messages.HostConfig
 import io.vertx.core.json.JsonObject
 
 /**
@@ -9,12 +13,32 @@ import io.vertx.core.json.JsonObject
 
 
 class DockerTask {
+    def dockerClient
+    DockerClient docker
+    ContainerCreation cctr
 
-    DockerTask(JsonObject cfg){
+    DockerTask(final Map cfg) {
+        docker = DefaultDockerClient.fromEnv().build();
+        Map pathBinds = cfg.binds
+        def hcfgb = HostConfig.builder()
+        pathBinds.each { k, v ->
+            hcfgb.appendBinds("${k}:${v}")  //support attributes like read only later
+        }
+        final HostConfig hcfg = hcfgb.build()
 
+        cctr = docker.createContainer(ContainerConfig.builder()
+                .image(cfg.image as String)
+                .exposedPorts(cfg.ports as Set<String>)
+                .volumes(cfg.volumes as Set<String>)
+                .hostConfig(hcfg)
+                .build())
+
+        //= new DefaultDockerClient("unix:///var/run/docker.sock");
+        // --or--
+        //.uri(URI.create("https://boot2docker:2376"))
+        // .dockerCertificates(new DockerCertificates(Paths.get("/Users/rohan/.docker/boot2docker-vm/")))
+        // .build();
     }
-
-    def dockerClient = new DockerClientImpl(System.env.DOCKER_HOST)
 
 
 }
