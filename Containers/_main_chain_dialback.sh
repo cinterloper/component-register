163fa1720cb276d60c2e1e4dc9c45a6684c786ee
Grant Haywood <grant@iowntheinter.net>
2016-08-28 12:58:21 -0700

    made dialback module to diffrent messging systems, add a helper to gen your jceks keystore for jwt

--- Containers/_main_chain_dialback.sh
+++ Containers/_main_chain_dialback.sh
@@ -1,16 +1,27 @@
 #listen on a channel named by our registration id, for commands from the bus.
 cd /opt/lash/lib/chain/
 CFG_PATH=/opt/lash/lib/chain/config/
+
+function send_message() {
+  vxc -c $CORNERSTONE_HOST:7000 -n $RETURN_ADDR
+}
+function listen_channels() {
+  vxc -c $CORNERSTONE_HOST:7000 -l -n $LAUNCHID
+}
 source config/config.sh
+#^you can overide the send_message or listen_channels methods
+#this ends up including everything in /opt/lash/lib/chain/config/include.list
 source chain.sh
+
+
 export METHOD=EB DECODE_SILENT=TRUE
 
- vxc -c $CORNERSTONE_HOST:7000 -l -n $LAUNCHID | while read JSON_STRING
+  listen_channels | while read JSON_STRING
   do
    decodeJson && lookup_command && run_task
     if [ "$RETURN_ADDR" != "" ]
     then
-      KEY_SET="$OUTPUT_KEYS OUTPUT" encodeJson | vxc -c $CORNERSTONE_HOST:7000 -n $RETURN_ADDR
+      KEY_SET="$OUTPUT_KEYS OUTPUT" encodeJson | send_message
       unset RETURN_ADDR OUTPUT_KEYS OUTPUT
     fi
    reset_chain
---
+++ create-test-keystore.sh
@@ -0,0 +1,1 @@
+keytool -genseckey -keystore keystore.jceks -storetype jceks -storepass secret -keyalg HMacSHA256 -keysize 2048 -alias HS256 -keypass secret
