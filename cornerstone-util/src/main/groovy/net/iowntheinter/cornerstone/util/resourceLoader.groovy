dd1985df7f9e51e690f8348026d7f9de7fd8e649
grant <grant@iowntheinter.net>
2017-01-14 03:55:17 +0000

    not much

--- cornerstone-util/src/main/groovy/net/iowntheinter/cornerstone/util/resourceLoader.groovy
+++ cornerstone-util/src/main/groovy/net/iowntheinter/cornerstone/util/resourceLoader.groovy
@@ -4,14 +4,25 @@ package net.iowntheinter.cornerstone.util
  * Created by grant on 4/11/16.
  */
 class resourceLoader {
+    static String getResourceAsString(String name) {
+        return getResource(name)
+    }
 
     static String getResource(String name) {
         def classloader = (URLClassLoader) (Thread.currentThread().getContextClassLoader())
-        def cpth = classloader.findResource(name);
+        def cpth = classloader.findResource(name)
         if (cpth) {
             return (classloader.getResourceAsStream(name).getText())
         } else
             return (-1)
     }
 
+    static byte[] getResourceBytes(String name) {
+        def classloader = (URLClassLoader) (Thread.currentThread().getContextClassLoader())
+        def cpth = classloader.findResource(name)
+        if (cpth) {
+            return (classloader.getResourceAsStream(name).getBytes())
+        } else
+            return (-1)
+    }
 }
